18845f8007e2a6edb10b780250357075c841bb5c
Dylan Richardson <dylanrichardson1996@gmail.com>
2018-08-30 11:14:01 -0400

    Move index.html to public dir. Use express static to serve public

--- server.js
+++ server.js
@@ -1,33 +1,8 @@
-var http = require('http')
-  , fs   = require('fs')
-  , url  = require('url')
-  , port = 8080;
+const express = require('express');
+const app = express();
 
-var server = http.createServer (function (req, res) {
-  var uri = url.parse(req.url)
+const PORT = process.env.PORT || 8080;
 
-  switch( uri.pathname ) {
-    case '/':
-      sendFile(res, 'index.html')
-      break
-    case '/index.html':
-      sendFile(res, 'index.html')
-      break
-    default:
-      res.end('404 not found')
-  }
-})
+app.use(express.static('public'));
 
-server.listen(process.env.PORT || port);
-console.log('listening on 8080')
-
-// subroutines
-
-function sendFile(res, filename) {
-
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.end(content, 'utf-8')
-  })
-
-}
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
