303392cec47e697358ba69f9252c280694601d94
Dylan Richardson <dylanrichardson1996@gmail.com>
2018-08-30 11:28:35 -0400

    Redirect all routes to the root

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+
+// create the default server
 const app = express();
 
+// choose the port from the env or default
 const PORT = process.env.PORT || 8080;
 
+// serve the static index.html
 app.use(express.static('public'));
 
+// redirect any route not found in public to the root
+app.use((_, res) => res.redirect('/'));
+
+// start the server on the desired port
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
